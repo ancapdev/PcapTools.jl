563c7e1bca4a48818ff5b8eff2d630e905168fb2
Christian Rorvik <christian.rorvik@gmail.com>
2020-02-07 13:58:44 +0000

    Files generated by PkgTemplates

---
+++ src/PcapTools.jl
@@ -0,0 +1,5 @@
+module PcapTools
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PcapTools
+using Test
+
+@testset "PcapTools.jl" begin
+    # Write your own tests here.
+end
