e81c931e4f19a704b0b94aab5fa26b2af768e115
klf-gs <klf@graystonetrading.com>
2024-09-30 12:02:36 +0100

    Undo autoformat
    Co-authored-by: Christian Rorvik <christian.rorvik@gmail.com>

--- src/fcs.jl
+++ src/fcs.jl
@@ -63,7 +63,7 @@ end
 Recompute the FCS for a record.
 """
 function compute_fcs(x::PcapRecord)
-    data_no_fcs = UnsafeArray{UInt8,1}(x.data.pointer, x.data.size .- ETHERNET_FCS_LENGTH)
+    data_no_fcs = UnsafeArray{UInt8, 1}(x.data.pointer, x.data.size .- ETHERNET_FCS_LENGTH)
     GC.@preserve x CRC32.unsafe_crc32(data_no_fcs, length(data_no_fcs) % Csize_t, 0x00000000)
 end
 
